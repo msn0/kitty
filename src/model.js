9ec2416fa0fdf75355de27c9122066e7dbc33036
Michał Jezierski <ambinanitelo@gmail.com>
2014-05-27 23:16:57 +0200

    split kitty --> kitty + model + view

---
+++ src/model.js
@@ -0,0 +1,76 @@
+var Model = function (modelName, options) {
+  var that = this;
+  this.modelName = modelName;
+
+  var nextId = function () {
+    return Math.round(Math.random() * 100000000)
+  };
+
+  Kitty.Model[modelName] = function (options) {
+    var model = Kitty.Model[that.modelName];
+    model.prototype.modelProtoReference = that;
+    this.objid = nextId();
+    model.prototype.init = function () {
+
+      for (var key in model.defaults) {
+        if (model.defaults.hasOwnProperty(key)) {
+          this[key] = options.hasOwnProperty(key) ? options[key] : model.defaults[key];
+        }
+      }
+
+      model.views[this.objid] = [];
+      model.collection.push(this);
+
+    };
+    model.prototype.set = function (params) {
+      for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+          this[key] = params[key];
+        }
+      }
+      this.notify();
+    };
+    model.prototype.get = function (property) {
+      return this[property];
+    };
+    model.prototype.remove = function () {
+      for (var key in model.views[this.objid]) {
+        if (model.views[this.objid].hasOwnProperty(key)) {
+          model.views[this.objid][key].remove();
+        }
+      }
+      delete model.views[this.objid];
+      var removeIndex;
+      for (var i = 0; i < model.collection.length; i++) {
+        if (model.collection[i].objid === this.objid) {
+          removeIndex = i;
+          break;
+        }
+      }
+      model.collection.splice(removeIndex, 1);
+    };
+    model.prototype.notify = function () {
+      for (var key in model.views[this.objid]) {
+        if(model.views.hasOwnProperty(key)) {
+          model.views[this.objid][key]._render();
+        }
+      }
+    };
+    this.init();
+  };
+  Kitty.Model[modelName].collection = [];
+  Kitty.Model[modelName].views = {};
+  Kitty.Model[modelName].defaults = (function () {
+    var def = {};
+    for (var key in options) {
+      if (options.hasOwnProperty(key)) {
+        def[key] = options[key];
+      }
+    }
+    return def;
+  })();
+  return Kitty.Model[modelName];
+
+};
+
+Kitty.Model = Model;
---
+++ src/view.js
@@ -0,0 +1,33 @@
+var View = function (viewName, options) {
+
+  Kitty.View[viewName] = function (data) {
+    var that = this;
+    this.model = data;
+    this.init = function () {
+      for (var key in options) {
+        if (options.hasOwnProperty(key)) {
+          this[key] = options[key];
+        }
+      }
+      var modRef = this.model.modelProtoReference;
+      Kitty.Model[modRef.modelName].views[this.model.objid].push(this);
+      this.el = document.createElement(options.tag);
+      this.container.appendChild(this.el);
+      return this;
+
+    };
+    this._render = function () {
+      this.el.innerHTML = this.template.render(this.model);
+      options.hasOwnProperty('events') && options.events.call(that);
+      options.hasOwnProperty('render') && options.render.call(that);
+      return this;
+    };
+    this.remove = function () {
+      this.el.parentNode.removeChild(this.el);
+    };
+    this.init()._render();
+  };
+  return Kitty.View[viewName];
+};
+
+Kitty.Model = Model;
